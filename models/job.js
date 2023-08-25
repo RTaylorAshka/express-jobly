6fc69f4ca858b33317a5528fd4a5590423c55ead
rtaylorashka <r.taylorashka@gmail.com>
2023-08-24 22:00:33 -0700

    Added Job model

---
+++ models/job.js
@@ -0,0 +1,167 @@
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const { sqlForPartialUpdate, sqlForFiltering } = require("../helpers/sql");
+
+
+/** Related functions for companies. */
+
+class Job {
+    /** Create a job (from data), update db, return new job data.
+     *
+     * data should be { title, salary, equity, companyHandle }
+     *
+     * Returns { id, title, salary, equity, companyHandle }
+     *
+     * Throws BadRequestError if company already in database.
+     * */
+
+    static async create({ title, salary, equity, companyHandle }) {
+        const companyCheck = await db.query(
+            `SELECT handle
+           FROM companies
+           WHERE handle = $1`,
+            [companyHandle]);
+
+        if (!(duplicateCheck.rows[0]))
+            throw new BadRequestError(`Company not in database: ${handle}`);
+
+        const result = await db.query(
+            `INSERT INTO jobs
+           (title, salary, equity, company_handle)
+           VALUES ($1, $2, $3, $4)
+           RETURNING id, title, salary, equity, company_handle AS "companyHandle"`,
+            [
+                title,
+                salary,
+                equity,
+                companyHandle
+            ],
+        );
+        const job = result.rows[0];
+
+        return company;
+    }
+
+    /** Find all jobs.
+     *
+     * Returns [{ id, title, salary, equity, companyHandle }, ...]
+     * */
+
+    static async findAll(queryData) {
+
+
+        const queryRef = {
+            title: {
+                typeValidator: (d) => { return typeof d == 'string' },
+                sqlComparitor: (d) => { return `title ILIKE '%${d}%'` },
+                expectedErr: "expected string from title parameter"
+
+            },
+            minSalary: {
+                typeValidator: (d) => { return !isNaN(d) },
+                sqlComparitor: (d) => { return `salary >= ${d}` },
+                expectedErr: "expecter number from minSalary parameter"
+
+            },
+            maxEmployees: {
+                typeValidator: (d) => { return !isNaN(d) },
+                sqlComparitor: (d) => { return `num_employees <= ${d}` },
+                expectedErr: "expecter number from minEmployee parameter"
+
+            }
+        }
+
+
+
+        const sqlFilter = sqlForFiltering(queryData, queryRef);
+        console.log(sqlFilter)
+
+        const jobsRes = await db.query(
+            `SELECT id,
+                  title,
+                  salary,
+                  company_handle AS "companyHandle"
+           FROM jobs
+           ${sqlFilter}
+           ORDER BY companyHandle `);
+        return jobsRes.rows;
+    }
+
+    /** Given a job id, return data about job.
+     *
+     * Returns { id, title, salary, equity, companyHandle }
+     *
+     * Throws NotFoundError if not found.
+     **/
+
+    static async get(id) {
+        const jobRes = await db.query(
+            `SELECT id,
+                  title,
+                  salary,
+                  company_handle AS "companyHandle"
+           FROM jobs
+           WHERE id = $1`,
+            [id]);
+
+        const job = jobRes.rows[0];
+
+        if (!job) throw new NotFoundError(`No job with id: ${id}`);
+
+        return job;
+    }
+
+    /** Update job data with `data`.
+     *
+     * This is a "partial update" --- it's fine if data doesn't contain all the
+     * fields; this only changes provided ones.
+     *
+     * Data can include: { title, salary, equity }
+     *
+     * Returns { id, title, salary, equity, companyHandle }
+     *
+     * Throws NotFoundError if not found.
+     */
+
+    static async update(id, data) {
+        const { setCols, values } = sqlForPartialUpdate(data);
+        const idVarIdx = "$" + (values.length + 1);
+
+        const querySql = `UPDATE companies 
+                      SET ${setCols} 
+                      WHERE id = ${idVarIdx} 
+                      RETURNING id, 
+                                title, 
+                                salary, 
+                                equity, 
+                                company_handle AS "companyHandle"`;
+        const result = await db.query(querySql, [...values, id]);
+        const job = result.rows[0];
+
+        if (!job) throw new NotFoundError(`No job with id: ${id}`);
+
+        return job;
+    }
+
+    /** Delete given job from database; returns undefined.
+     *
+     * Throws NotFoundError if company not found.
+     **/
+
+    static async remove(id) {
+        const result = await db.query(
+            `DELETE
+           FROM jobs
+           WHERE id = $1
+           RETURNING id`,
+            [id]);
+        const job = result.rows[0];
+
+        if (!job) throw new NotFoundError(`No job with id: ${id}`);
+    }
+}
+
+
+module.exports = Job;
